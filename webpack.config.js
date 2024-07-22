1840a9600015c1993535071111a2d1947444ac11
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-07-22 17:03:35 +0530

    initial file setup

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins:[new htmlWebpackPlugin({
+    template: './src/index.html',
+  })]
+};
